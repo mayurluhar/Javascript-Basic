02b12b6f250c6625a0ffe85a8d3da84f2d447587
Mayur Luhar <83022097+mayur778@users.noreply.github.com>
2022-08-12 15:34:11 +0530

    chabge

--- LCO_files/03 functions in JS/04_hoisting_lyst2895.js
+++ LCO_files/03 functions in JS/04_hoisting_lyst2895.js
@@ -1,9 +1,9 @@
-tipper("80");
+// tipper("80");
 
-function tipper(a) {
-  var bill = parseInt(a);
-  console.log(bill + 5);
-}
+// function tipper(a) {
+//   var bill = parseInt(a);
+//   console.log(bill + 5);
+// }
 
 var bigTipper = function (a) {
   var bill = parseInt(a);
